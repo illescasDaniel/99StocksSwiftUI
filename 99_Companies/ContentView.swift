e5ef5aa0072a3ca775c236b24194807774e1fed1
Daniel Illescas Romero <illescas.daniel@protonmail.com>
2019-06-07 18:24:15 +0200

    Initial Commit

---
+++ 99_Companies/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  99_Companies
+//
+//  Created by Daniel Illescas Romero on 07/06/2019.
+//  Copyright © 2019 Daniel Illescas Romero. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
---
+++ 99_CompaniesTests/_9_CompaniesTests.swift
@@ -0,0 +1,34 @@
+//
+//  _9_CompaniesTests.swift
+//  99_CompaniesTests
+//
+//  Created by Daniel Illescas Romero on 07/06/2019.
+//  Copyright © 2019 Daniel Illescas Romero. All rights reserved.
+//
+
+import XCTest
+@testable import _9_Companies
+
+class _9_CompaniesTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
